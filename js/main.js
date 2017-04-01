1473f681766c1621deaebf9de468a89db1b280c3
OlgaPatsalay <helga_pats@mail.ru>
2017-04-02 00:30:28 +0300

    Инициализация

---
+++ js/main.js
@@ -0,0 +1,15 @@
+;(function ($) {
+
+    $(function () {
+        $('.slider-cont').slick({
+            // autoplay: true,
+            autoplaySpeed: 10000,
+            dots: false,
+            slidesToShow: 3,
+            slidesToScroll: 1
+
+
+
+        });
+    });
+})(jQuery);
